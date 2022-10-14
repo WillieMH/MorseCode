7c59dde3f09676356723cce2dbd2b859a0e709df
Willie Henderson <wm2727@ou.ac.uk>
2022-10-14 11:32:47 +0100

    tranlating - need to deal with spacing & phrases

--- js/morseFunctions.js
+++ js/morseFunctions.js
@@ -1,7 +1,77 @@
+// MAIN FUNCTION START
 const translate = (englishWord) => {
   return "***";
 }
+// MAIN FUNCTION END
 
-console.log(translate("s"));
+      // PSEUDO
+        // get input
+        // split input - tick
+        // change to lowercase - tick
+        // loop through each word to translate
+        // rejoin words
+        // display words
 
+// split sentence funtion
+const splitUserInput = (userInput) => {
+  const convertToNonCaps = userInput.toLowerCase();
+  splitArry = convertToNonCaps.split(" ");
+  return splitArry;
+};
+
+// loop through word in array and translate
+const newWord = (theWord) => {
+  return theWord
+    .replaceAll("a", " *- ")
+    .replaceAll("b", " -*** ")
+    .replaceAll("c", " -*-* ")
+    .replaceAll("d", " -** ")
+    .replaceAll("e", " * ")
+    .replaceAll("f", " **-* ")
+    .replaceAll("g", " --* ")
+    .replaceAll("h", " **** ")
+    .replaceAll("i", " ** ")
+    .replaceAll("j", " *--- ")
+    .replaceAll("k", " -*-* ")
+    .replaceAll("l", " *-** ")
+    .replaceAll("m", " -- ")
+    .replaceAll("n", " -* ")
+    .replaceAll("o", " --- ")
+    .replaceAll("p", " *--* ")
+    .replaceAll("q", " --*- ")
+    .replaceAll("r", " *-* ")
+    .replaceAll("s", " ... ")
+    .replaceAll("t", " - ")
+    .replaceAll("u", " **- ")
+    .replaceAll("v", " ***- ")
+    .replaceAll("w", " *-- ")
+    .replaceAll("x", " -**- ")
+    .replaceAll("y", " -*-- ")
+    .replaceAll("z", " --** ")
+    .replaceAll("0", " ----- ")
+    .replaceAll("1", " *---- ")
+    .replaceAll("2", " **--- ")
+    .replaceAll("3", " ***-- ")
+    .replaceAll("4", " ****- ")
+    .replaceAll("5", " ***** ")
+    .replaceAll("6", " -**** ")
+    .replaceAll("7", " --*** ")
+    .replaceAll("8", " ---** ")
+    .replaceAll("9", " ----* ")
+
+}
+
+console.log(newWord("sos"));
+
+console.log(splitUserInput("I LIKE SHOUTING"));
+
+
+
+
+
+
+
+
+
+// Don't touch
 module.exports = {translate};
